2c8166dbc45a211aaad5c6a4da989b4dd7994f3e
hjgode <hjgode@gmail.com>
2016-08-01 06:45:24 +0200

    added EXTER "C" unmangling

--- uart.h
+++ uart.h
@@ -106,79 +106,85 @@ LICENSE:
 #define UART_BUFFER_OVERFLOW  0x0200              /**< @brief receive ringbuffer overflow */
 #define UART_NO_DATA          0x0100              /**< @brief no receive data available   */
 
+#ifdef __cplusplus
+ extern "C" {
+#endif
 
-/*
-** function prototypes
-*/
-
-/**
-   @brief   Initialize UART and set baudrate 
-   @param   baudrate Specify baudrate using macro UART_BAUD_SELECT()
-   @return  none
-*/
-extern void uart_init(unsigned int baudrate);
-
-
-/**
- *  @brief   Get received byte from ringbuffer
- *
- * Returns in the lower byte the received character and in the 
- * higher byte the last receive error.
- * UART_NO_DATA is returned when no data is available.
- *
- *  @return  lower byte:  received byte from ringbuffer
- *  @return  higher byte: last receive status
- *           - \b 0 successfully received data from UART
- *           - \b UART_NO_DATA           
- *             <br>no receive data available
- *           - \b UART_BUFFER_OVERFLOW   
- *             <br>Receive ringbuffer overflow.
- *             We are not reading the receive buffer fast enough, 
- *             one or more received character have been dropped 
- *           - \b UART_OVERRUN_ERROR     
- *             <br>Overrun condition by UART.
- *             A character already present in the UART UDR register was 
- *             not read by the interrupt handler before the next character arrived,
- *             one or more received characters have been dropped.
- *           - \b UART_FRAME_ERROR       
- *             <br>Framing Error by UART
- */
-extern unsigned int uart_getc(void);
-
-
-/**
- *  @brief   Put byte to ringbuffer for transmitting via UART
- *  @param   data byte to be transmitted
- *  @return  none
- */
-extern void uart_putc(unsigned char data);
-
-
-/**
- *  @brief   Put string to ringbuffer for transmitting via UART
- *
- *  The string is buffered by the uart library in a circular buffer
- *  and one character at a time is transmitted to the UART using interrupts.
- *  Blocks if it can not write the whole string into the circular buffer.
- * 
- *  @param   s string to be transmitted
- *  @return  none
- */
-extern void uart_puts(const char *s );
-
-
-/**
- * @brief    Put string from program memory to ringbuffer for transmitting via UART.
- *
- * The string is buffered by the uart library in a circular buffer
- * and one character at a time is transmitted to the UART using interrupts.
- * Blocks if it can not write the whole string into the circular buffer.
- *
- * @param    s program memory string to be transmitted
- * @return   none
- * @see      uart_puts_P
- */
-extern void uart_puts_p(const char *s );
+  /*
+  ** function prototypes
+  */
+  
+  /**
+     @brief   Initialize UART and set baudrate 
+     @param   baudrate Specify baudrate using macro UART_BAUD_SELECT()
+     @return  none
+  */
+  extern void uart_init(unsigned int baudrate);
+  
+  
+  /**
+   *  @brief   Get received byte from ringbuffer
+   *
+   * Returns in the lower byte the received character and in the 
+   * higher byte the last receive error.
+   * UART_NO_DATA is returned when no data is available.
+   *
+   *  @return  lower byte:  received byte from ringbuffer
+   *  @return  higher byte: last receive status
+   *           - \b 0 successfully received data from UART
+   *           - \b UART_NO_DATA           
+   *             <br>no receive data available
+   *           - \b UART_BUFFER_OVERFLOW   
+   *             <br>Receive ringbuffer overflow.
+   *             We are not reading the receive buffer fast enough, 
+   *             one or more received character have been dropped 
+   *           - \b UART_OVERRUN_ERROR     
+   *             <br>Overrun condition by UART.
+   *             A character already present in the UART UDR register was 
+   *             not read by the interrupt handler before the next character arrived,
+   *             one or more received characters have been dropped.
+   *           - \b UART_FRAME_ERROR       
+   *             <br>Framing Error by UART
+   */
+  extern unsigned int uart_getc(void);
+  
+  
+  /**
+   *  @brief   Put byte to ringbuffer for transmitting via UART
+   *  @param   data byte to be transmitted
+   *  @return  none
+   */
+  extern void uart_putc(unsigned char data);
+  
+  
+  /**
+   *  @brief   Put string to ringbuffer for transmitting via UART
+   *
+   *  The string is buffered by the uart library in a circular buffer
+   *  and one character at a time is transmitted to the UART using interrupts.
+   *  Blocks if it can not write the whole string into the circular buffer.
+   * 
+   *  @param   s string to be transmitted
+   *  @return  none
+   */
+  extern void uart_puts(const char *s );
+  
+  
+  /**
+   * @brief    Put string from program memory to ringbuffer for transmitting via UART.
+   *
+   * The string is buffered by the uart library in a circular buffer
+   * and one character at a time is transmitted to the UART using interrupts.
+   * Blocks if it can not write the whole string into the circular buffer.
+   *
+   * @param    s program memory string to be transmitted
+   * @return   none
+   * @see      uart_puts_P
+   */
+  extern void uart_puts_p(const char *s );
+  #ifdef __cplusplus
+ }
+#endif
 
 /**
  * @brief    Macro to automatically put a string constant into program memory
